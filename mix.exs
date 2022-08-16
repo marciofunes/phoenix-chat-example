0dc02f9aa28576f3e9aa92fe8e5ef54b136f7946
Márcio Funes <marciofunes@gmail.com>
2022-08-16 17:37:48 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chat.Mixfile do
     [
       app: :chat,
       version: "1.6.2",
-      elixir: "~> 1.12.3",
+      elixir: "~> 1.9.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
